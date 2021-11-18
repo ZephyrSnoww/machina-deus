7eceb52a9c096035254dbb3cebc0cff17b0ed191
Toaster <ttoasterrr@gmail.com>
2021-11-17 21:23:52 -0700

    Added simple voting command

---
+++ bot/commands/vote.js
@@ -0,0 +1,23 @@
+const { createDefaultEmbed } = require("../helpers/defaults")
+
+module.exports = {
+    data: {
+        name: "vote",
+        arguments: "<description>",
+        description: "Create a simple vote."
+    },
+
+    async execute(client, message) {
+        const embed = createDefaultEmbed(
+            `${message.author.username} Started a Vote`,
+            message.content.substring(client.config.prefix.length + this.data.name.length)
+        );
+
+        const reply = await message.reply({ embeds: [embed] });
+
+        await reply.react("yes:910737740377759774");
+        await reply.react("no:910737778386534460");
+
+        console.info(`${message.author.username} started a simple vote.`);
+    }
+}
